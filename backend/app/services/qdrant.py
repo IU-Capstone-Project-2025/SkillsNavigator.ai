afb5759baf1e2aa1900868ab260d7b68b83e4f07
Bulat Gazizov <pizza1093@gmail.com>
2025-06-22 22:21:52 +0300

    feat: faster course search, rating and authors field

--- backend/app/services/qdrant.py
+++ backend/app/services/qdrant.py
@@ -6,6 +6,8 @@ from qdrant_client.conversions import common_types as types
 from app.services import run_blocking, encoder
 
 import httpx
+import asyncio
+import traceback
 
 
 class QdrantService:
@@ -31,7 +33,8 @@ class QdrantService:
     async def loadCourses(self):
         print("Loading Courses from stepik", flush=True)
         async with httpx.AsyncClient(timeout=None) as client:
-            courses = []
+            courses = {}
+            courses_ids_set = set()
             points = []
             page = 1
             while True:
@@ -43,64 +46,97 @@ class QdrantService:
 
                 for i in range(len(courses_list["course-lists"])):
                     course_section = courses_list["course-lists"][i]
-                    for z in range(len(course_section["courses"]) // 100 + 1):
-                        params = {'ids[]': course_section["courses"][100 * z:100 * (z + 1)]}
-                        print("Getting courses in " + course_section["title"], flush=True)
-                        courses_info_req = await client.get("https://stepik.org/api/courses", params=params)
-                        courses_info = courses_info_req.json()["courses"]
-                        # ratings_info_req = await client.get(f"https://stepik.org/api/course-review-summaries", params=params)
-                        # ratings_info = ratings_info_req.json()["course-review-summaries"]
-                        try:
-                            for k in range(len(courses_info)):
-                                course_info = courses_info[k]
-                                # rating_info = ratings_info[k]
-                                courses.append({
-                                    # Payload
-                                    "id": course_info["id"],
-                                    "cover_url": course_info["cover"],
-                                    "title": course_info["title"],
-                                    "duration": course_info["time_to_complete"],
-                                    "difficulty": course_info["difficulty"],
-                                    "price": 0 if course_info["price"] is None else course_info["price"],
-                                    "currency_code": course_info["currency_code"],
-                                    "pupils_num": course_info["learners_count"],
-                                    "authors": [], # парсить авторов
-                                    "rating": 5, # парсить рейтинг
-                                    "url": f"https://stepik.org/course/{course_info['id']}/promo",
-                                    "description": course_info["description"],
-                                    "summary": course_info["summary"],
-                                    "target_audience": course_info["target_audience"],
-                                    "acquired_skills": ''.join(course_info["acquired_skills"]),
-                                    "acquired_assets": ''.join(course_info["acquired_assets"]),
-                                    "title_en": course_info["title_en"],
-                                    "learning_format": course_info["learning_format"],
-                                    # "section_desc": course_section["description"],
-                                })
-                        except Exception as e:
-                            print(e, flush=True)
-                    for idx, course in enumerate(courses):
-                        vector = await encoder.vectorize(f"Название: {course['title']} ({course['title_en']})\n"
-                                                         f"Сложность: {course['difficulty']}\n"
-                                                         f"Резюме: {course['summary']}")
+                    print("Getting courses in " + course_section["title"], flush=True)
+                    courses_ids_set.update(course_section["courses"])
 
-                        # Create a point for the course
-                        points.append(models.PointStruct(
-                            id=course["id"],
-                            vector=vector,
-                            payload=course
-                        ))
+                print(f"Current size: {len(courses_ids_set)}", flush=True)
 
-                    self.client.upload_points(
-                        collection_name="courses",
-                        points=points,
-                    )
-                    courses.clear()
-                    points.clear()
                 if not courses_list['meta']['has_next']:
                     break
                 page += 1
 
-            print("Creating collection", flush=True)
+            # We have list of all stepik courses. Now we need to get info about them.
+
+            courses_ids_list = list(courses_ids_set)
+
+            for i in range(0, len(courses_ids_list), 100):
+                print(f"Page: {i}", flush=True)
+                subset = courses_ids_list[i:i + 100]
+                params = {'ids[]': subset}
+                courses_info_req = await client.get("https://stepik.org/api/courses", params=params)
+                courses_info = courses_info_req.json()["courses"]
+                    # ratings_info_req = await client.get(f"https://stepik.org/api/course-review-summaries", params=params)
+                    # ratings_info = ratings_info_req.json()["course-review-summaries"]
+                review_ids = []
+                author_ids = []
+                try:
+                    for k in range(len(courses_info)):
+                        course_info = courses_info[k]
+                        if len(course_info["authors"]) > 0:
+                            author_ids.append(course_info["authors"][0])
+                        review_ids.append(course_info["review_summary"])
+                        # rating_info = ratings_info[k]
+                        courses[course_info["id"]] = {
+                        # Payload
+                        "id": course_info["id"],
+                        "cover_url": course_info["cover"],
+                        "title": course_info["title"],
+                        "duration": course_info["time_to_complete"],
+                        "difficulty": course_info["difficulty"],
+                        "price": 0 if course_info["price"] is None else course_info["price"],
+                        "currency_code": course_info["currency_code"],
+                        "pupils_num": course_info["learners_count"],
+                        "authors":  course_info["authors"][0] if len(course_info["authors"]) > 0 else "", # парсить авторов
+                        "rating": 5,
+                        "url": f"https://stepik.org/course/{course_info['id']}/promo",
+                        "description": course_info["description"],
+                        "summary": course_info["summary"],
+                        "target_audience": course_info["target_audience"],
+                        "acquired_skills": ''.join(course_info["acquired_skills"]),
+                        "acquired_assets": ''.join(course_info["acquired_assets"]),
+                        "title_en": course_info["title_en"],
+                        "learning_format": course_info["learning_format"],
+                        # "section_desc": course_section["description"],
+                        }
+
+                    print(f"Getting reviews", flush=True)
+                    params = {'ids[]': review_ids}
+                    reviews = (await client.get("https://stepik.org/api/course-review-summaries", params=params)).json()
+                    for review_c in range(len(reviews["course-review-summaries"])):
+                        review = reviews["course-review-summaries"][review_c]
+                        courses[review["course"]]["rating"] = review["average"]
+
+                    print(f"Getting authors", flush=True)
+                    params = {'ids[]': author_ids}
+                    authors = (await client.get("https://stepik.org/api/users", params=params)).json()
+                    for author_c in range(len(authors["users"])):
+                        for i in courses:
+                            if courses[i]["authors"] == authors["users"][author_c]["id"]:
+                                courses[i]["authors"] = authors["users"][author_c]["full_name"]
+                                break      
+
+                except Exception as e:
+                    print(e, flush=True)
+                    print(traceback.format_exc(), flush=True)    
+
+                print(f"Start vectorization", flush=True)
+                for course in courses.values():
+                    vector = await encoder.vectorize(f"Название: {course['title']} ({course['title_en']})\n"
+                        f"Сложность: {course['difficulty']}\n"
+                        f"Резюме: {course['summary']}")
+                        # Create a point for the course
+                    points.append(models.PointStruct(
+                        id=course["id"],
+                        vector=vector,
+                        payload=course
+                    ))
+                print("Uploading..", flush=True)
+                self.client.upload_points(
+                    collection_name="courses",
+                    points=points,
+                    )
+                courses.clear()
+                points.clear()
 
         print('Courses loaded', flush=True)
 
