0a84e3b511a0f54b905afdcd0d59a9b31127efaf
sayfetik <sayfetik2005@gmail.com>
2025-06-25 15:06:47 +0300

    Save backend settings

--- backend/app/main.py
+++ backend/app/main.py
@@ -30,6 +30,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=["*"],  # или укажите ["http://localhost:8080"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
